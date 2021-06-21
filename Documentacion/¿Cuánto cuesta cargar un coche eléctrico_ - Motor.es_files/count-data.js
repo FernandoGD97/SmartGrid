cf91965782731bdf379a07ee868995cfc37d7245
FernandoLapTop <fgallegodonoso@gmail.com>
2021-06-21 10:49:02 +0200

    deleting doc

--- Documentacion/¿Cuánto cuesta cargar un coche eléctrico_ - Motor.es_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"y","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"https:\/\/www.motor.es\/disqus\/67580","comments":0}]});
-}
